168a148a8c29b6beb0cd26959f5c9f289e9b357e
Shaun Baer <shaun.baer@gmail.com>
2016-12-06 11:01:44 -0500

    Apollo build

---
+++ apollo-master/app/themes/mission-control/templates/global/footer.php
@@ -0,0 +1,8 @@
+<footer class="global-footer" role="contentinfo">
+  <div class="container">
+    <div class="global-footer-wrap">
+      <p class="global-footer-copyright">&copy; <?= date('Y') . ' ' . get_bloginfo('name') ?> </p>
+    </div>
+  </div>
+</footer>
+
